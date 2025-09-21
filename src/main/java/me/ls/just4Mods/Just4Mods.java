20c5863ca6ea88d883dc561cbd8c8c761e6cf058
Land64 <tagg5126@gmail.com>
2025-09-21 14:47:18 -0400

    Using maven

---
+++ src/main/java/me/ls/just4Mods/Just4Mods.java
@@ -0,0 +1,17 @@
+package me.ls.just4Mods;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Just4Mods extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
